5ea1a734dbdc42ae0fa0994511e0d89f35beac9e
Noah Nelson <noah.james.nelson@gmail.com>
2023-05-14 18:00:44 -0400

    added local storag and other hours

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
 $(document).ready(function () {
 
   let currentDateTime = dayjs().format("MMMM D, YYYY");
@@ -34,7 +30,7 @@ $(document).ready(function () {
     localStorage.setItem(time, value);
   });
 
-  $("#hour-09 .description").val(localStorage.getItem("09"));
+  $("#hour-09 .description").val(localStorage.getItem("9"));
   $("#hour-10 .description").val(localStorage.getItem("10"));
   $("#hour-11 .description").val(localStorage.getItem("11"));
   $("#hour-12 .description").val(localStorage.getItem("12"));
@@ -45,46 +41,3 @@ $(document).ready(function () {
   $("#hour-17 .description").val(localStorage.getItem("17"));
 
 });
-
- // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-
-
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-
-  
-  // TODO: Add code to display the current date in the header of the page.
-
-
-// ^ All that stuff up there is the same stuff as below...
-// ALL THIS CODE I SPENT 1 HOUR WATCHING A WHOLE DANG VIDEO JUST FOR HIM TO TELL ME THE EASIER WAY HSFKHVDKFHVKDFHKVD
-  // function formatDate(dateObject) {
-  //const parts = {
-  //  date: dateObject.getDate(),
-  //  month: dateObject.getMonth() +1,
-  //  year: dateObject.getFullYear(),
-  //  hour: (dateObject.getHours() % 12) || 12,
-  //  minute: dateObject.getMinutes().toString().padStart(2, "0"),
-  //  amOrPm: dateObject.getHours() < 12 ? "AM" : "PM"
-  //};
-
-  //console.log(parts);
-  //return `${parts.date}/${parts.month}/${parts.year} ${parts.hour}:${parts.minute} ${parts.amOrPm}`;
-//}
-
-  //const myDate = new Date();
-  //const myDateFormatted = formatDate(myDate);
-
-  //console.log(myDateFormatted);
